497028a2c79531b95f16c0fe4592169306a1d1e5
asmaa <moulayasmaa2004@gmail.com>
2024-05-25 20:30:17 +0100

    aaaa

---
+++ database/migrations/2024_05_24_220013_create_tablemapping_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tablemapping', function (Blueprint $table) {
+            $table->id();
+            $table->string('layer')->nullable();
+            $table->string('target_table_name')->nullable();
+            $table->string('mapping_name')->nullable();
+            $table->string('main_source')->nullable();
+            $table->string('main_source_alias')->nullable();
+            $table->string('join, 10000')->nullable();
+            $table->string('filter_criterion')->nullable();
+            $table->string('historization_algorithm')->nullable();
+            $table->string('historization_columns')->nullable();
+            $table->string('source')->nullable();
+            $table->string('remarque')->nullable();
+            $table->string('filename')->nullable();
+            $table->datetime('imported_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tablemapping');
+    }
+};
---
+++ resources/views/excel/services.blade.php
@@ -0,0 +1,91 @@
+
+<html style="font-size: 16px;" lang="fr"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Accueil.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 6.9.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/default-logo.png"
+    }  
+</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Accueil">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/">
+</head>
+
+<body data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="fr">
+    <header class="u-clearfix u-header u-header" id="sec-7a97">
+    
+    
+    </header>
+    
+    <section class="u-clearfix u-custom-color-1 u-section-2" id="carousel_b31b">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-expanded-width-sm u-expanded-width-xs u-opacity u-opacity-55 u-shape u-shape-svg u-text-palette-2-light-1 u-shape-1">
+          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xlink:href="#svg-2739"></use></svg>
+          <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-2739" style="enable-background:new 0 0 160 160;"><path d="M151.7,132.5c-3.4,6-7.9,11.7-13.3,16.5c-29.2,26.1-54.1-2.3-88.2-3S0,109.5,0,91.6s14.8-31.3,25.3-45.3S33.4,11,51.8,3
+	c15.3-6.6,33.6-0.4,51.6,0.7c11.2,0.7,28.8-3.8,38.2,0.4s11.8,15.4,9.2,22.8c-1.9,5.4-5.5,11.6-12.8,16.5
+	c-9.3,10.1-4.7,22.9,3.7,33.4c7.8,9.7,17.7,11.7,18.3,23.5C160.4,110.8,157.6,122.2,151.7,132.5L151.7,132.5z"></path></svg>
+        </div>
+        <div class="data-layout-selected u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-size-22 u-size-60-md">
+                <div class="u-layout-col">
+                  <div class="u-align-left u-container-style u-layout-cell u-size-60 u-white u-layout-cell-1">
+                    <div class="u-container-layout u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-container-layout-1">
+                      <h3 class="u-text u-text-1">SQL Reporting Software</h3>
+                      <p class="u-text u-text-2">Our flexible and powerful platform also enables our SSRS customers to build custom connectors to data sources including the ability to connect to your own internal databases.</p>
+                      <a href="#" class="u-active-palette-3-base u-border-1 u-border-palette-3-base u-btn u-button-style u-hover-palette-3-base u-none u-text-active-white u-text-hover-white u-text-palette-3-base u-btn-1">learn more</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-size-38 u-size-60-md">
+                <div class="u-layout-col">
+                  <div class="u-size-40">
+                    <div class="u-layout-row">
+                      <div class="u-align-left u-container-style u-image u-layout-cell u-size-60 u-image-1" data-image-width="5000" data-image-height="3159">
+                        <div class="u-container-layout u-container-layout-2"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="u-size-20">
+                    <div class="u-layout-row">
+                      <div class="u-align-left u-container-style u-custom-color-3 u-layout-cell u-size-30 u-layout-cell-3">
+                        <div class="u-container-layout u-valign-middle u-container-layout-3">
+                          <h3 class="u-text u-text-3">Excel Dashboard</h3>
+                          <p class="u-text u-text-4">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                        </div>
+                      </div>
+                      <div class="u-align-left u-container-style u-image u-layout-cell u-size-30 u-image-2" data-image-width="900" data-image-height="789">
+                        <div class="u-container-layout u-container-layout-4"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+     
+      </div>
+    </section>
+    
+    
+    
+    
+    
+</body></html>
